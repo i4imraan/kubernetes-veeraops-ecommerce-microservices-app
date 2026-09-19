5ab519e87fa72721a5c17a6d4d51d4d8ce5fc472
i4imraan <ikhan7246@gmail.com>
2026-09-19 09:48:30 +0530

    Change S3 bucket name for Terraform backend

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevops"
+    bucket       = "devi4imraan"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
